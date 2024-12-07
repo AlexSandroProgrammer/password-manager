c6d9e437472bc8b45e5e7a392ca108aba3eed3a3
alexsandro2005 <luisalejandrm533@gmail.com>
2024-12-06 23:37:44 -0500

    Update HeaderMain

--- app/(routes)/(home)/components/HeaderMain/HeaderMain.tsx
+++ app/(routes)/(home)/components/HeaderMain/HeaderMain.tsx
@@ -25,15 +25,32 @@ import {
 //* importacion de iconos
 import { ChevronDown, Icon } from "lucide-react";
 import { dataHeaderMain } from "./HeaderMain.data";
+import { useState } from "react";
 
 export function HeaderMain() {
+  // * creamos un estado para manejar el tipo de elemento
+  const [typeElement, setTypeElement] = useState<"password" | "folder" | "">();
+  //* creamos un estado para manejar la abiertura del dialogo
+  const [openDialog, setOpenDialog] = useState(false);
+  //* creamos un estado para manejar la abiertura del menu dropdown
+  const [openDropdown, setOpenDropdown] = useState(false);
+
+  //* creamos los metodos para abrir y cerrar los dialogos y el menu dropdown
+  const closeDialogAndDropdown = () => {
+    setOpenDialog(false);
+    setOpenDropdown(false);
+  };
+
+  console.log(typeElement);
+
+  //* creamos el componente para la accion de crear un nuevo elemento
   return (
     <div className="flex justify-between items-center">
       <h1 className="text-xl md:text-3xl font-semibold">
         Todas las cajas fuertes
       </h1>
-      <Dialog>
-        <DropdownMenu>
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        <DropdownMenu open={openDropdown} onOpenChange={setOpenDropdown}>
           <DropdownMenuTrigger asChild>
             <Button>
               Nueva <ChevronDown />
@@ -49,7 +66,7 @@ export function HeaderMain() {
                       key={typeElement}
                       className="justify-start"
                       variant="ghost"
-                      onClick={() => console.log("Cambio de tipo boton")}
+                      onClick={() => setTypeElement(typeElement)}
                     >
                       <Icon className="w-4 h-4 mr-2" />
                       {text}
@@ -60,6 +77,13 @@ export function HeaderMain() {
             </DropdownMenuLabel>
           </DropdownMenuContent>
         </DropdownMenu>
+        <DialogContent className="sm:max-w-[825px]">
+          <DialogHeader>
+            <DialogTitle>Nuevo Elemento</DialogTitle>
+          </DialogHeader>
+          {typeElement === "password" && <p>Form Password</p>}
+          {typeElement === "folder" && <p>Form Folder</p>}
+        </DialogContent>
       </Dialog>
     </div>
   );
